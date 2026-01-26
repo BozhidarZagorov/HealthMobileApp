f635f4d8d9dcf9c884c595b6703d762c7d02e53f
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-26 16:04:54 +0200

    add utills for injection and resupply saving

---
+++ mobileApp/utils/injectionLog.ts
@@ -0,0 +1,17 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { nanoid } from 'nanoid'
+
+export const STORAGE_KEYS = {
+  INJECTION_LOG: 'injection_log',
+  MONTH_SCHEDULE: 'month_schedule',
+  RESUPPLY: 'first_resupply_date',
+  INJECTION: 'first_injection_date',
+}
+
+// Schedule item used in memory
+export type ScheduleItem = {
+  id: string
+  date: Date
+  type: 'injection' | 'resupply'
+  done?: boolean
+}
