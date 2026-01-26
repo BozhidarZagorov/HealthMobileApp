0c409a14483e9993a2aa775fe54cb7975c2fbb28
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-26 16:07:16 +0200

    add page for history

--- mobileApp/app/(tabs)/_layout.tsx
+++ mobileApp/app/(tabs)/_layout.tsx
@@ -26,10 +26,18 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'This Month',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explorehistory"
+        options={{
+          title: 'History',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
+    
   );
 }
