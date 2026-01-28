f699c5378d04239e6a3f54f367d7b6e79c2ecf14
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-28 22:24:09 +0200

    import notifications to the app

--- mobileApp/app/_layout.tsx
+++ mobileApp/app/_layout.tsx
@@ -1,16 +1,34 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
+import { useEffect } from 'react'
+import * as Notifications from 'expo-notifications'
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from '@/hooks/use-color-scheme'
+import { requestNotificationPermission } from '@/utils/notifications'
 
 export const unstable_settings = {
   anchor: '(tabs)',
-};
+}
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    // New recommended fields
+    shouldShowAlert: true,    // still works, optional
+    shouldShowBanner: true,   // required now
+    shouldShowList: true,     // required now
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+})
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
+
+  useEffect(() => {
+    requestNotificationPermission()
+  }, [])
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
@@ -20,5 +38,5 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
-  );
+  )
 }
